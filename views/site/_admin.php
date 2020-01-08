e65b317f0d80cc58434fc08a628e919d4c1d3c78
bitsnaps <bitsnaps@yahoo.fr>
2020-01-08 09:47:32 +0100

    update input filters layout in admin homepage

--- views/site/_admin.php
+++ views/site/_admin.php
@@ -25,88 +25,70 @@ foreach ($exercices as $exercice){
     }
 }
 ?>
-<div class="row">
-    <div class="page-header">
-        <h4>Tableau de board: <?= count($exercices)>0?(Yii::$app->formatter->format($exercices[0]->rapport->debut, ['date', 'format' => 'yyyy'])):'<aucun>' ?></h4>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-12">
-    <?php $form = ActiveForm::begin(['layout' => 'horizontal'/*, 'method' => 'get'*/]) ?>
-    <!--<div class="col-md-3">-->
-        <!--<label class="control-label" for="rapport_id">Rapport:</label>-->
-        <?= $form->field($filterModel, 'rapport_id')->widget(Select2::classname(), [
-            'data' => $rapports,
-            'options' => [
-                'class' => 'btn btn-primary',
-                'placeholder' => 'Sélectionner un rapport...'
-                ],
-            'pluginOptions' => [
-                'allowClear' => true
-            ],
-        ]) ?>
-        <?php /*echo kartik\select2\Select2::widget([
-            'name' => 'rapport_id',
-            'data'=> $rapports,
-            'options' => [
-                'placeholder' => 'Sélectionner le rapport'                    
-            ],
-            'pluginOptions' => [
-                'allowClear' => true
-            ],
-            'pluginEvents' => [
-               "select2:select" => "function(data) { updateTable(data.params.data); }",
-            ]
-        ])*/ ?>
-    <!--</div>-->
-    <!--<div class="col-md-3">-->
-        <!--<label class="control-label" for="unite_id">Unité:</label>-->
-        <?= $form->field($filterModel, 'unite_id')->widget(Select2::classname(), [
-            'data' => $unites,
-            'options' => ['placeholder' => 'Sélectionner l\'unité...'],
-            'pluginOptions' => [
-                'allowClear' => true
-            ],
-        ]) ?>        
-        <?php /*echo kartik\select2\Select2::widget([
-            'name' => 'unite_id',
-            'data'=> $unites,
-            'options' => [
-                'placeholder' => 'Sélectionner l\'unité'                    
-            ],
-            'pluginOptions' => [
-                'allowClear' => true
-            ],                
-        ])*/ ?>
-    <!--</div>-->
-    <!--<div class="col-md-3">-->
-        <!--<label class="control-label" for="canevas_id">Canevas:</label>-->
-        <?= $form->field($filterModel, 'canevas_id')->widget(Select2::classname(), [
-            'data' => $canevas,
-            'options' => ['placeholder' => 'Sélectionner le canevas...'],
-            'pluginOptions' => [
-                'allowClear' => true
-            ],
-        ]) ?>         
-        <?php /*echo kartik\select2\Select2::widget([
-            'name' => 'canevas_id',
-            'data'=> $canevas,
-            'options' => [
-                'placeholder' => 'Sélectionner le canevas'                    
-            ],
-            'pluginOptions' => [
-                'allowClear' => true
-            ],                
-        ])*/ ?>        
-        
-    <!--</div>-->
-    <div class="col-md-6 col-md-offset-3">
-        <div class="form-group pull-right">  
-            <?= Html::submitButton(Yii::t('app', 'Filtrer'), ['class' => 'btn btn-success']) ?>
+    <div class="row">
+        <div class="page-header">
+            <h4>Tableau de board: <?= count($exercices)>0?(Yii::$app->formatter->format($exercices[0]->rapport->debut, ['date', 'format' => 'yyyy'])):'<aucun>' ?></h4>
         </div>
     </div>
-    <?php $form::end(); ?>
-</div>
+    <div class="row">
+        <div class="col-md-12">
+            <?php $form = ActiveForm::begin([
+                'layout' => 'horizontal',
+                'fieldConfig' => [
+                    'template' => "<div class=\"form-md-line-input\">{beginWrapper}\n{input}<div class=\"form-control-focus\"> </div>\n{error}\n</div>{endWrapper}",
+                    'horizontalCssClasses' => [
+                        'label' => '',
+                        'offset' => '',
+                        'wrapper' => 'col-sm-12',
+                        'error' => 'has-error',
+                        'hint' => 'help-block',
+                    ],
+                ],                
+                /*, 'method' => 'get'*/
+                ]) ?>
+        <div class="row">
+            <div class="col-md-4">
+                <?= $form->field($filterModel, 'rapport_id', ['options' => ['class'=> 'col-sm-12'] ])->widget(Select2::classname(), [
+                    'data' => $rapports,
+                    'options' => [
+                        'placeholder' => 'Sélectionner un rapport...'
+                        ],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ],
+                ])->label(false) ?>
+            </div>
+            <div class="col-md-4">
+                <!--<label class="control-label" for="unite_id">Unité:</label>-->
+                <?= $form->field($filterModel, 'unite_id')->widget(Select2::classname(), [
+                    'data' => $unites,
+                    'options' => ['placeholder' => 'Sélectionner l\'unité...'],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ],
+                ])->label(false) ?>        
+
+            </div>
+            <div class="col-md-4">
+                <?= $form->field($filterModel, 'canevas_id')->widget(Select2::classname(), [
+                    'data' => $canevas,
+                    'options' => ['placeholder' => 'Sélectionner le canevas...'],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ],
+                ])->label(false) ?>
+            </div>
+        </div><!-- .row -->
+        <div class="row">
+            <div class="col-md-12 pull-right">
+                <div class="form-group pull-right">  
+                    <?= Html::submitButton(Yii::t('app', 'Filtrer'), ['class' => 'btn btn-success']) ?>
+                </div>
+            </div>
+        </div><!-- .row -->
+        <?php $form::end(); ?>
+        </div>
+    </div><!-- .row -->
 <br>
 <div class="row">
     <div class="col-md-12">
